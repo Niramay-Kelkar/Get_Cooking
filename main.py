5274a22549729ba2e42e957f536003114d666369
Niramay-Kelkar <kelkar.n@husky.neu.edu>
2022-05-06 06:20:00 -0400

    Adding Dockerfile

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
 import pandas as pd
+from Food_Search import *
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return{'message':'Hello World!'}
+
+@app.get("/ingredients/{ingredients}/number/{num}")
+def getRecommendations(ingredients, num):
+    return get_pred(ingredients, num)
+    #return recipe
+
+
+def get_pred(ingredients, number):
+    ingred = ingredients.split(", ")
+    n_rec = int(number)
+    recipe = search_ingredients(ingred , n_rec)
+    recipe['recipe_urls'] = recipe['recipe_urls'].apply(make_clickable)
+    #df = pd.read_json(recipe)
+    #recipe = recipe.to_html(escape=False)
+    #print(df)
+    print(type(recipe))
+    return recipe
+
+def make_clickable(link):
+    # target _blank to open new window
+    # extract clickable text to display for your link
+    text = link.split('/')[5]
+    return f'<a target="_blank" href="{link}">{text}</a>'
